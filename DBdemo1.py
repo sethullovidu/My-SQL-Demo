05b75aa2082fb0c308245fa19d3f8d2645c0c994
sethullovidu <sethullovidu77@gmail.com>
2025-08-30 15:03:45 +0530

    Update DBdemo1.py

--- DBdemo1.py
+++ DBdemo1.py
@@ -1,24 +1,20 @@
-import mysql.connector
-
-# Try to connect
-conn = mysql.connector.connect(
-    host="localhost",
-    user="root",  
-    password="1234",
-    port=3307
-)
-
-print("Connected?", conn.is_connected())
-cur = conn.cursor()
-
-
-db_name = "Student"
-
-cur.execute(f"CREATE DATABASE IF NOT EXISTS {db_name}")
-print(f"Database ready: {db_name}")
-
-
-cur.execute(f"USE {db_name}")
-
-cur.close()
-conn.close()
+import mysql.connector 
+DB_NAME = "school" 
+conn = mysql.connector.connect( 
+host="localhost", 
+user="root", 
+password="1234", 
+port=3307,
+database=DB_NAME 
+) 
+cur = conn.cursor() 
+cur.execute(""" 
+CREATE TABLE IF NOT EXISTS students ( 
+id INT AUTO_INCREMENT PRIMARY KEY, 
+name VARCHAR(50) NOT NULL, 
+age INT 
+) ENGINE=InnoDB 
+""") 
+print("Table ready: students") 
+cur.close() 
+conn.close() 
