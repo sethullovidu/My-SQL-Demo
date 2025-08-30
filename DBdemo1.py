eea99d92c4c9387a3624886a395d7af24aca22e5
sethullovidu <sethullovidu77@gmail.com>
2025-08-30 13:01:06 +0530

    update

--- DBdemo1.py
+++ DBdemo1.py
@@ -5,10 +5,20 @@ conn = mysql.connector.connect(
     host="localhost",
     user="root",  
     password="1234",
-    port= 3307
+    port=3307
 )
 
-print("Connection Successful?", conn.is_connected())
+print("Connected?", conn.is_connected())
+cur = conn.cursor()
 
-# Close connection
+
+db_name = "Student"
+
+cur.execute(f"CREATE DATABASE IF NOT EXISTS {db_name}")
+print(f"Database ready: {db_name}")
+
+
+cur.execute(f"USE {db_name}")
+
+cur.close()
 conn.close()
