750ea14ed40b4fcc018b8a246627331d3f0e20c5
sethullovidu <sethullovidu77@gmail.com>
2025-08-30 11:02:47 +0530

    Update DBdemo1.py

--- DBdemo1.py
+++ DBdemo1.py
@@ -1,1 +1,13 @@
 import mysql.connector
+
+# Try to connect
+conn = mysql.connector.connect(
+    host="localhost",
+    user="root",          # replace if you use another user
+    password="YOUR_PASSWORD"  # replace with your real MySQL password
+)
+
+print("Connection Successful?", conn.is_connected())
+
+# Close connection
+conn.close()
